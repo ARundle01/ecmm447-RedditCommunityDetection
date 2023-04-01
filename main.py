4729f05def67a19f09cc45b0b434e608d50983ce
Alex Rundle <alexrundle01@gmail.com>
2023-04-01 12:36:29 +0100

    Added TSV formatting
    Added function to format the original dataset from TSV format to CSV format, as well as to save only the relevant fields to a separate CSV.
    
    In this repo, only the CSV containing the relevant fields is supplied, as the original dataset is very large. Link to the original dataset can be found in the README

--- main.py
+++ main.py
@@ -4,5 +4,26 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
+def format_tsv(tsv_path):
+    """
+    Function to format the Reddit Hyperlink dataset from Tab-Separated format
+    to Comma-Separated format. Following conversion, the CSV is reduced to contain
+    only fields relevant to this project
+
+    :param tsv_path: Path to TSV file
+    :return: None
+    """
+    df = pd.read_table(tsv_path, sep="\t")
+    df.to_csv("./data/soc-redditHyperlinks-body.csv")
+
+    reddit_csv = "./data/soc-redditHyperlinks-body.csv"
+
+    reddit_df = pd.read_csv(reddit_csv)
+
+    reddit_df = reddit_df[["SOURCE_SUBREDDIT", "TARGET_SUBREDDIT"]]
+
+    reddit_df.to_csv("./data/redditHyperlinks-subredditsOnly.csv")
+
+
 if __name__ == '__main__':
     pass
