f9f2deaf06d13baf9db36de8f522d857184a685d
Alex Rundle <alexrundle01@gmail.com>
2023-04-03 17:52:42 +0100

    Louvain and Girvan-Newman
    Added better Louvain detection and started working on Girvan-Newman.
    
    Also, updated report.

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import numpy as np
 import pandas
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
 import networkx as nx
 import urllib.request as req
 import os.path as path
+import igraph
+
+import community
 
 from collections.abc import Iterable
 from networkx.algorithms import community as comm
@@ -142,9 +147,44 @@ def make_weighted(df: pandas.DataFrame) -> networkx.classes.graph.Graph:
 
 
 def perform_louvain(graph: networkx.classes.graph.Graph, resolution: float = 1.0) -> list:
+    print("Performing Louvain Community Detection...")
     louvain = comm.louvain_communities(graph, weight='WEIGHT', resolution=resolution)
 
+    print("Done!")
     return louvain
+
+
+def perform_girvan(graph: networkx.classes.graph.Graph):
+    print("Performing Girvan-Newman Community Detection...")
+    girvan = comm.girvan_newman(graph)
+
+    print("Done!")
+    return girvan
+
+
+def draw_clu(graph, position, labels, algorithm, savename):
+    clusters = np.array(list(set(labels.values())))
+
+    nodes = nx.draw_networkx_nodes(graph, position, node_size=250,
+                                   cmap=mcolors.ListedColormap(plt.cm.Set3(clusters)),
+                                   node_color=list(labels.values()),
+                                   nodelist=list(labels.keys()))
+
+    graph_labels = nx.draw_networkx_labels(graph, position, font_size=9)
+
+    edges = nx.draw_networkx_edges(graph, position, alpha=0.5)
+
+    plt.title(algorithm)
+
+    cb = plt.colorbar(nodes, ticks=range(0, len(clusters)), label='Communities')
+    cb.ax.tick_params(length=0)
+
+    cb.set_ticklabels(list(set(labels.values())))
+
+    nodes.set_clim(-0.5, len(clusters)-0.5)
+
+    plt.axis('off')
+    plt.savefig(savename)
 
 
 if __name__ == '__main__':
@@ -158,29 +198,63 @@ if __name__ == '__main__':
     reddit_csv = "./data/redditHyperlinks-subredditsOnly.csv"
     reddit_df = load_df(reddit_csv)
 
+    more_than_one_df = reddit_df[reddit_df['WEIGHT'] > 1]
+    more_than_two_df = more_than_one_df[more_than_one_df['WEIGHT'] > 2]
+
     # destiny_df = _get_nodes_to_target("destinythegame", reddit_df)
     # destiny_df = _get_nodes_combined(["destinythegame", "raidsecrets"], reddit_df)
 
-    weighted = make_weighted(reddit_df)
+    # unweighted = make_unweighted(reddit_df)
+    # unweighted_df = nx.to_pandas_edgelist(unweighted)
+    reddit = make_weighted(reddit_df)
+    more_than_one = make_weighted(more_than_one_df)
+    more_than_two = make_weighted(more_than_two_df)
 
-    all_neighbours = nx.all_neighbors(weighted, "destinythegame")
-    neigh_list = []
+    # all_neighbours = nx.all_neighbors(weighted, "destinythegame")
+    # neigh_list = []
 
-    for neighbour in all_neighbours:
-        neigh_list.append(neighbour)
+    # for neighbour in all_neighbours:
+    #     neigh_list.append(neighbour)
 
-    directed = weighted.to_directed()
+    # directed = weighted.to_directed()
 
-    succ_list = []
-    successors = directed.successors("destinythegame")
+    # succ_list = []
+    # successors = directed.successors("destinythegame")
 
-    for successor in successors:
-        succ_list.append(successor)
+    # for successor in successors:
+    #     succ_list.append(successor)
 
-    directed_df = nx.to_pandas_edgelist(directed)
+    # directed_df = nx.to_pandas_edgelist(directed)
     # destinygraph = make_unweighted(destiny_df)
 
-    louvain = perform_louvain(weighted, resolution=0.1)
+    # pos = nx.spring_layout(weighted, weight='WEIGHT')
+    labels_reddit = community.best_partition(reddit)
+    labels_mto = community.best_partition(more_than_one)
+    labels_mtt = community.best_partition(more_than_two)
+
+    pos = nx.spring_layout(more_than_two, weight='WEIGHT')
+
+    # for key, value in labels_louvain.items():
+    #     if key == "destinythegame":
+    #         print(value)
+
+    draw_clu(more_than_two, pos, labels_mtt, 'Louvain', savename='./out/louvain_mtt.png')
+
+    # girvan = perform_girvan(weighted)
+    # print("Performing Girvan-Newman Community Detection...")
+    # girvan = comm.girvan_newman(unweighted)
+    # girvan_list = []
+
+    # print("Making Girvan Community list...")
+    # girvan_list = list(girvan)
+    # i = 0
+    # t = len(list(girvan))
+    # for community in girvan:
+    #     print(f"Adding Community Number {i} / {t}...")
+    #     girvan_list.append(list(community))
+    #     print("Added!")
+    #     i += 1
+    # print("Done!")
 
     # nx.draw_spring(destinygraph, with_labels=False)
     # plt.savefig("./out/destiny.png", dpi=1200)
